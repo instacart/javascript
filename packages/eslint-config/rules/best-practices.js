0890e6f531701d585034ba003d09a41d189b424f
Nathan <nathanmarks@users.noreply.github.com>
2019-04-08 11:07:21 -0400

    Remove deprecated rule and disable another (#5)

--- packages/eslint-config/rules/best-practices.js
+++ packages/eslint-config/rules/best-practices.js
@@ -17,7 +17,7 @@ module.exports = {
   'class-methods-use-this': 'off',
 
   // require return statements to either always or never specify values
-  'consistent-return': 'error',
+  'consistent-return': 'off',
 
   // specify curly brace conventions for all control statements
   curly: ['error', 'multi-line'],
