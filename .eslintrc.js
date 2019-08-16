f93a5632d50d6f430790530adb187ceae2417483
Nathan <nathanmarks@users.noreply.github.com>
2019-08-16 10:03:55 -0400

    Add precommit hook for linting (#17)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,11 @@
 module.exports = {
   extends: ['@instacart/eslint-config'],
+  overrides: [
+    {
+      files: ['*.spec.ts', '*.spec.js', '*.spec.tsx', '**/test/**'],
+      rules: {
+        'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
+      },
+    },
+  ],
 }
--- packages/tsconfig/test/fixture.tsx
+++ packages/tsconfig/test/fixture.tsx
@@ -1,16 +1,16 @@
-import React from 'react';
+import React from 'react'
 
 interface Props {
-  text: string;
-  hidden?: boolean;
+  text: string
+  hidden?: boolean
 }
 
 const FixtureComponent: React.FunctionComponent<Props> = ({ text, hidden }) => {
   if (hidden) {
-    return null;
+    return null
   }
 
-  return <div>{text}</div>;
+  return <div>{text}</div>
 }
 
-export { FixtureComponent };
+export { FixtureComponent }
